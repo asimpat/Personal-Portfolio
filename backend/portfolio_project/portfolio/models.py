f955c8745d7e0f88b0f780290fe8fe2b3a5f3c42
Asim <asimokon@yahoo.com>
2025-08-03 17:35:10 +0100

    updated model ans serializers

--- backend/portfolio_project/portfolio/models.py
+++ backend/portfolio_project/portfolio/models.py
@@ -90,10 +90,6 @@ class Contact(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
 
-    @property
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
-
 
 class Resume(models.Model):
     """Model for resume file"""
