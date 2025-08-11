e4400e68b0b58b86558b866fa7554ad4b462000c
Asim Patrick <asim@axumtechnologies.com>
2025-08-11 17:44:54 +0100

    model updated

--- backend/portfolio_project/portfolio/models.py
+++ backend/portfolio_project/portfolio/models.py
@@ -18,7 +18,7 @@ class Skill(models.Model):
 
 
 class Project(models.Model):
-    """Model for portfolio projects"""
+  
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='projects/', blank=True, null=True)
