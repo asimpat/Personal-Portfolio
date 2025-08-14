8bf0270f671aa809748ce743130c7af4a5b0d9da
Asim <asimokon@yahoo.com>
2025-08-14 08:45:59 +0100

    fix email error

--- backend/portfolio_project/portfolio_project/settings.py
+++ backend/portfolio_project/portfolio_project/settings.py
@@ -107,14 +107,7 @@ DATABASES = {
 #     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
 #     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 #     'ROTATE_REFRESH_TOKENS': True,
-#     'BLACKLIST_AFTER_ROTATION': True,
-#     'UPDATE_LAST_LOGIN': True,
-#     'ALGORITHM': 'HS256',
-#     'SIGNING_KEY': SECRET_KEY,
-#     'VERIFYING_KEY': None,
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-#     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-#     'USER_ID_FIELD': 'id',
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
