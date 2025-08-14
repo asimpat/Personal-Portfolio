f4f891da312dac218102816f0b06f9fae82cdfcd
Asim <asimokon@yahoo.com>
2025-08-14 08:45:48 +0100

    fix email error

--- backend/portfolio_project/portfolio_project/settings.py
+++ backend/portfolio_project/portfolio_project/settings.py
@@ -115,13 +115,7 @@ DATABASES = {
 #     'AUTH_HEADER_TYPES': ('Bearer',),
 #     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
 #     'USER_ID_FIELD': 'id',
-#     'USER_ID_CLAIM': 'user_id',
-#     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-#     'TOKEN_TYPE_CLAIM': 'token_type',
-# }
 
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
