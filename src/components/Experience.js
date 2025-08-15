8d2b0e19ba6b88935ac04322db0119ee4960384a
Asim <asimokon@yahoo.com>
2025-08-15 08:54:16 +0100

    frontend integration

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -58,7 +58,7 @@ export const Experience = () => {
               <div className="experience-cta">
                 <button
                   onClick={() => {
-                 
+            
                     const link = document.createElement("a");
                     link.href = "http://127.0.0.1:8000/api/resume/";
                     link.setAttribute("download", "Asim's_Resume.pdf"); 
