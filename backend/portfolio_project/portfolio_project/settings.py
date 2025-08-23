c0b2c042835e113b217fa633dd0dc196eb81fdfe
Asim <asimokon@yahoo.com>
2025-08-23 23:47:41 +0100

    frontend integration

--- backend/portfolio_project/portfolio_project/settings.py
+++ backend/portfolio_project/portfolio_project/settings.py
@@ -136,7 +136,7 @@ MEDIA_ROOT = BASE_DIR/'media'
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000", 
+    "http://127.0.0.1:3000",  
 ]
 
 # Default primary key field type
