d40d9948a210bbfb8514f278019f0a2191dd7a60
Asim <asimokon@yahoo.com>
2025-08-25 23:50:54 +0100

    frontend integration

--- backend/portfolio_project/portfolio_project/settings.py
+++ backend/portfolio_project/portfolio_project/settings.py
@@ -133,7 +133,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",  
 ]
-
+ 
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
